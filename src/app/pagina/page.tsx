b9cc841065cb4f8104de1fe361677435a169b351
Andrey Cadmo <andreycadmo06@gmail.com>
2024-07-13 17:11:04 -0300

    colocando em pastas (externas e internas)

--- src/app/pagina/page.tsx
+++
@@ -1,9 +0,0 @@
-import Pagina from "@/components/layout/Pagina";
-
-export default function Page() {
-    return (
-        <Pagina>
-            <div>Exemplo Básico de uma página!!!</div>
-        </Pagina>
-    )
-}
