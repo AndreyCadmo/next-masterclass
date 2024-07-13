e0d49db9684888a6244b7298c5201e9c94966904
Andrey Cadmo <andreycadmo06@gmail.com>
2024-07-13 16:36:48 -0300

    criando a pagina

---
+++ src/app/pagina/page.tsx
@@ -0,0 +1,9 @@
+import Pagina from "@/components/layout/Pagina";
+
+export default function Page() {
+    return (
+        <Pagina>
+            <div>Exemplo Básico de uma página!!!</div>
+        </Pagina>
+    )
+}
